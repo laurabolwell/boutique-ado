7e16c1bc9a15177bd6ef540727005f8d9a4d6e93
laurabolwell <laurabolwell@hotmail.com>
2024-01-31 13:59:38 +0000

    Finish add product functionality

--- products/forms.py
+++ products/forms.py
@@ -8,9 +8,10 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+
 def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    categories = Categories.objects.all()
+    categories = Category.objects.all()
     friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
     self.fields['category'].choices = friendly_names
